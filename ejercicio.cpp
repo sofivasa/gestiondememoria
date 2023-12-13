0898522d7382901a543a3db82c7b87749d98ccdc
sofivasa <sofivasa@myuax.com>
2023-12-13 17:32:33 +0100

    funcionmemoria

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -6,3 +6,13 @@
 #include <sys/wait.h>
 #define SIZE 4096
 
+int main() {
+    // Creamos funcion para memoria compartida con mmap
+    char *shared_memory = (char *)mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    //
+    if (shared_memory == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+
+  
+}
